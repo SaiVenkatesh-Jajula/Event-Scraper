330b16056e3119078ad17d1d7d5ff9b8a6b86259
saivenkatesh <saivenkatesh619@gmail.com>
2024-02-27 11:28:59 +0530

    Implemented with oop paradigm

--- backend.py
+++ backend.py
@@ -3,38 +3,46 @@ import selectorlib
 import mailing
 import sqlite3
 
-connection = sqlite3.connect("demo.db")
-cursor = connection.cursor()
 
-def scrape(url):
-    """Scrape the page source from the url"""
-    response = requests.get(url)
-    source = response.text
-    return source
+class Event:
+    def scrape(self, url):
+        """Scrape the page source from the url"""
+        response = requests.get(url)
+        source = response.text
+        return source
 
-def extract(source):
-    """Extracting required information"""
-    extraction = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    information = extraction.extract(source)['events']
-    return information
+    def extract(self, source):
+        """Extracting required information"""
+        extraction = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        information = extraction.extract(source)['events']
+        return information
 
-def sendingmail(info):
-    content = f"""\
-Subject: Event Scheduled please check the Ticket Portal.
-    
-Event details - {info}
-    """
-    mailing.mailing(content)
 
-def reading():
-    # with open("database.txt",'r') as file:
-    #     return file.read()
-    cursor.execute('select * from musicevent')
-    eventslist = cursor.fetchall()
-    return eventslist
-def appending(newitem):
-    # with open("database.txt",'a') as file:
-    #     file.writelines(information+'\n')
-    cursor.execute("insert into musicevent values(?)",newitem)
-    connection.commit()
+class Mail:
+    def sendingmail(self, info):
+        content = f"""\
+    Subject: Event Scheduled please check the Ticket Portal.
+        
+    Event details - {info}
+        """
+        mailing.mailing(content)
 
+
+class DB:
+    def __init__(self, dbname):
+        self.connection = sqlite3.connect(dbname)
+
+    def reading(self):
+        # with open("database.txt",'r') as file:
+        #     return file.read()
+        cursor = self.connection.cursor()
+        cursor.execute('select * from musicevent')
+        eventslist = cursor.fetchall()
+        return eventslist
+
+    def appending(self, newitem):
+        # with open("database.txt",'a') as file:
+        #     file.writelines(information+'\n')
+        cursor = self.connection.cursor()
+        cursor.execute("insert into musicevent values(?)", newitem)
+        self.connection.commit()
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-import backend
+from backend import *
 import time
 
+event = Event()
+mail = Mail()
+db = DB(dbname="demo.db")
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
 while True:
-    source = backend.scrape(URL)
-    information = backend.extract(source)
-    database = backend.reading()
+    source = event.scrape(URL)
+    information = event.extract(source)
+
+    database = db.reading()
     rows = [i[0] for i in database]
     print(rows)
     if information != 'No upcoming tours':
@@ -14,9 +18,8 @@ while True:
         newitem = information.split(',')
         transformed_info = "".join(newitem).strip()
         if transformed_info not in rows:
-             backend.sendingmail(information)
-             temptuple += (transformed_info,)
-             backend.appending(temptuple)
+            mail.sendingmail(information)
+
+            temptuple += (transformed_info,)
+            db.appending(temptuple)
     time.sleep(2)
-
-
